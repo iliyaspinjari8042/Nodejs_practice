e26e9c308d1c96ec3b007b863e48d99af85a16cb
Shaikh Iliyas <129730921+iliyaspinjari8042@users.noreply.github.com>
2024-03-25 07:52:03 +0000

    Data type

---
+++ Basic/second.js
@@ -0,0 +1,16 @@
+const accountId=123456;
+let emialAddress='iliyaspinjari8042@gail.com';
+let UserAddress='khirdi khurd tal raveer distict jalgaon'
+console.table(['accountId','emialAddress','UserAddress']);
+"use strict";//treat all js code as newer version
+//main webssite tc39.es
+//datatype
+/**
+ * number=> 2 to the power 53
+ * BigInt
+ * String
+ * boolean
+ * null=>standalone value  // typeof object
+ * undefine => undifine
+ * symbol => unique
+ */
