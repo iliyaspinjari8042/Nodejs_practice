cca39e0376e28e95c7d44046d92a6aaaa860be6b
Shaikh Iliyas <129730921+iliyaspinjari8042@users.noreply.github.com>
2024-03-25 15:13:17 +0000

    some conversion operation

---
+++ Basic/conversion-Operation.js
@@ -0,0 +1,28 @@
+// let number1="iliyas"
+// let number2=" pinjari"
+// let result=number1+number2;
+// console.log(result);
+// let practice='1'+1;//11
+// let practice1=1+'1';//11
+// let practice3=1+'1'+1;//111
+// let practice4=1+'1'+1+1;//1111
+// let practice5=1+1+'1'+1+1;//2111
+// let practice6='1'+1+1;//111
+// console.log(practice6)
+// console.log(+true)//1
+// console.log(+false)//0
+// console.log(+"")//0
+// console.log(null>0);//false
+// console.log(null===0);//false
+// console.log(null<0);//false
+// console.log(null>=0);//true
+// console.log(null<=0);//true
+/**
+ * null > 0: When comparing null with a number using the greater than (>) operator, null is treated as 0. Since 0 is not greater than 0, the result is false.
+null == 0: Even though null behaves like 0 when compared with numeric values, the strict equality operator (===) considers null and 0 as different data types. Therefore, the result is false.
+null < 0: Similar to the first case, null is treated as 0 when compared with a number using the less than (<) operator. Since 0 is not less than 0, the result is false.
+null >= 0: Since null is treated as 0 when compared with numeric values, and 0 is greater than or equal to 0, the result is true.
+null <= 0: Again, null is treated as 0 when compared with numeric values, and 0 is less than or equal to 0, so the result is true.
+ */
+// console.log('2'>1)//true
+// console.log('02'>1)//true
