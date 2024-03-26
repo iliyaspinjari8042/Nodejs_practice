330d86a690db3dd27ac186ccef3e7606392ae5d4
Shaikh Iliyas <129730921+iliyaspinjari8042@users.noreply.github.com>
2024-03-26 16:55:28 +0000

    What is array and there Methods

---
+++ 02_Basic/01_Array.js
@@ -0,0 +1,36 @@
+//An array in JavaScript is a data structure used to store multiple values in a single variable. 
+let newArr=[1,2,3,4,5];
+let newArr2=['iliyas','kasim','noor'];
+let newArr3=new Array(1,2,3,4,5);
+// console.log(newArr3);
+newArr.push(10);//add element in last
+// console.log(newArr)
+newArr.pop();//remove last element
+// console.log(newArr)
+newArr.unshift(10);//add element in first
+// console.log(newArr)
+newArr.shift();//remove element in first
+// console.log(newArr)
+//console.log(newArr.includes(4))//4 available or not in given array return boolean=>true
+//console.log(newArr.includes(8))//8 available or not in given array return boolean=>false
+// console.log(newArr.length)//length of array =>5
+// console.log(newArr[1])//fetch element index 1=>2
+//console.log(newArr.indexOf(3));//whoses element in index number 3=>2
+// console.log(newArr.concat([6,7,8,9]));
+
+// console.log(newArray)
+// let newOne =newArray.slice(2);//['noor','sultan','salman','Touhid']
+// console.log(newOne);
+// let newOne =newArray.slice(2,5);//['noor','sultan','salman']
+// console.log(newOne);
+// let newArray=['iliyas','kasim','noor','sultan','salman','Touhid'];
+// console.log('A' ,newArray)//A [ 'iliyas', 'kasim', 'noor', 'sultan', 'salman', 'Touhid' ]
+// console.log('A ka Ans ',newArray.slice(2,5));//A ka Ans  [ 'noor', 'sultan', 'salman' ]
+// console.log('B' ,newArray)//B [ 'iliyas', 'kasim', 'noor', 'sultan', 'salman', 'Touhid' ]
+// console.log(newArray.splice(2,5));//[ 'noor', 'sultan', 'salman', 'Touhid' ]
+// console.log('C' ,newArray)//C [ 'iliyas', 'kasim' ]
+const fruits = ['apple', 'banana', 'orange', 'kiwi'];
+fruits.splice(2, 1, 'lemon'); // Removes 1 element at index 2 and adds 'lemon'
+console.log(fruits); 
+
+
